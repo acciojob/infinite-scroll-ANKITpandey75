9ad3adc92f9b2781297c2690ae76265189342aed
ANKITpandey75 <125044396+ANKITpandey75@users.noreply.github.com>
2026-07-01 11:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+const list = document.getElementById("infi-list");
 
+let count = 1;
+
+// Add initial 10 items
+for (let i = 0; i < 10; i++) {
+    const li = document.createElement("li");
+    li.innerText = `Item ${count++}`;
+    list.appendChild(li);
+}
+
+// Infinite scroll
+list.addEventListener("scroll", function () {
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight - 1) {
+        for (let i = 0; i < 2; i++) {
+            const li = document.createElement("li");
+            li.innerText = `Item ${count++}`;
+            list.appendChild(li);
+        }
+    }
+});
